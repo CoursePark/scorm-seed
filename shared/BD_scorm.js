25d816ef058a9b8f4d6298b0dfcadf393fc32d90
Donal Mac An Ri <donal@coursepark.com>
2013-11-01 13:36:02 -0230

    expanded the API to cover common use cases

--- shared/BD_scorm.js
+++ shared/BD_scorm.js
@@ -17,9 +17,7 @@ var BLUEDROP = BLUEDROP || {};
             settings[option] = options[option];
         }
         
-        //options = options + defaults;
-        
-        scormWrapper.debug.isActive = options.debug;
+        scormWrapper.debug.isActive = settings.debug;
         
         var scorm, startTimestamp, endTimestamp;
         scorm = scormWrapper.SCORM;
@@ -31,49 +29,86 @@ var BLUEDROP = BLUEDROP || {};
         addEvent(settings.win, 'unload', function() {
             endTimestamp = new Date();
             if (settings.trackDuration) {
-                scorm.set("cmi.core.session_time", ConvertMilliSecondsToSCORMTime(endTimestamp - startTimestamp));
-                scorm.save();
+                setSessionTime(endTimestamp - startTimestamp);
             }
             scorm.quit();
         });
         
+        var cmi_elements = {
+            '1.2': {
+                'completion': 'cmi.core.lesson_status',
+                'success': 'cmi.core.lesson_status',
+                'bookmark': 'cmi.core.lesson_location',
+                'score': 'cmi.core.score.raw',
+                'learner': 'cmi.student_name',
+                'sessiontime': 'cmi.core.session_time',
+                'totaltime': 'cmi.core.total_time',
+                'mode': 'cmi.core.lesson_mode'
+            },
+            '2004': {
+                'completion': 'cmi.completion_status',
+                'success': 'cmi.success_status',
+                'bookmark': 'cmi.location',
+                'score': 'cmi.score.raw',
+                'learner': 'cmi.learner_name',
+                'sessiontime': 'cmi.core.session_time',
+                'totaltime': 'cmi.total_time',
+                'mode': 'cmi.mode'
+            }
+        }
+        
+        function setPassed() {
+            return scorm.set(cmi_elements[scorm.version]['success'], 'passed') && scorm.save();
+        },
+        function setFailed() {
+            return scorm.set(cmi_elements[scorm.version]['completion'], 'failed') && scorm.save();
+        },
+        
         return {
             getBookmark: function() {
-                if (scorm.version == "1.2") {
-                    return scorm.get("cmi.core.lesson_location");
-                }
+                return scorm.get(cmi_elements[scorm.version]['bookmark']);
             },
             setBookmark: function(bookmark) {
-                if (scorm.version == "1.2") {
-                    var success = scorm.set("cmi.core.lesson_location", bookmark);
-                    return success && scorm.save();
+                return scorm.set(cmi_elements[scorm.version]['bookmark'], bookmark) && scorm.save();
+            },
+            isComplete: function() {
+                return scorm.get(cmi_elements[scorm.version]['completion']) == 'completed' ||
+                    scorm.get(cmi_elements[scorm.version]['success']) == 'passed';
+            },
+            getSuccessStatus: function() {
+                var success = scorm.get(cmi_elements[scorm.version]['success']);
+                if (success != 'passed' && success != 'failed') {
+                    success = 'unknown';
                 }
-            },
-            getCompletionStatus: function() {
-                return scorm.status("get");
-            },
-            setCompletionStatus: function(completionStatus) {
-                var success = scorm.status("set", completionStatus);
-                return success && scorm.save();
+                return success;
             }
+            setComplete: function() {
+                return scorm.set(cmi_elements[scorm.version]['completion'], 'completed') && scorm.save();
+            },
+            setIncomplete: function() {
+                return scorm.set(cmi_elements[scorm.version]['completion'], 'incomplete') && scorm.save();
+            },
+            setPassed: setPassed,
+            setFailed: setFailed,
             getScore: function() {
-                if (scorm.version == "1.2") {
-                    return scorm.get("cmi.core.score.raw");
-                }
+                return scorm.get(cmi_elements[scorm.version]['score']);
             },
             setScore: function(score) {
-                if (scorm.version == "1.2") {
-                    var success = scorm.set("cmi.core.score.raw", score);
-                    
-                    if (success && options.masteryScore) {
-                        if (score > options.masteryScore) {
-                            success = scorm.status("set", "passed");
-                        } else {
-                            success = scorm.status("set", "failed");
-                        }
+                scorm.set(cmi_elements[scorm.version]['score'], score);
+                
+                if (options.masteryScore) {
+                    if (score > options.masteryScore) {
+                        return setPassed();
+                    } else {
+                        return setFailed();
                     }
-                    return success && scorm.save();
                 }
+            },
+            setSessionTime: function(seconds) {
+                return scorm.set(cmi_elements[scorm.version]['completion'], ConvertMilliSecondsToSCORMTime(seconds)) && scorm.save();
+            }
+            getTotalTime: function() {
+                return scorm.get(cmi_elements[scorm.version]['total_time']);
             }
         }
     }
